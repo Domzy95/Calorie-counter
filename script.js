d611776220f085e8c67a2dfb92b02f9c312873c8
Domen <domen.sinkovec@gmail.com>
2024-01-14 20:10:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// CALORIE COUNTER ELEMENT
+const calorieCounter = document.getElementById("calorie-counter");
+// BUDGET ELEMENT
+const budgetNumberInput = document.getElementById("budget");
+//ENTRY DROPDOWN ELEMENT
+const entryDropdown = document.getElementById("entry-dropdown");
+// ADD ENTRY
+const addEntryButton = document.getElementById("add-entry");
+//CLEAR ELEMENT
+const clearButton = document.getElementById("clear");
+//OUTPUT ELEMENT
+const output = document.getElementById("output");
+//IS ERROR
+let isError = false;
+// CLEAR INPUT STRING
+function cleanInputString(str) {}
